52a46c42244f0f6da720f15d1262a37adf48856d
DJeWG <jeremy.weicheldinger@orange.fr>
2017-02-20 15:23:58 +0100

    Mise à jour CSS

--- apps/messages.php
+++ apps/messages.php
@@ -1,8 +1,14 @@
 <?php
+$count = 0;
 $res = mysqli_query($db, "SELECT messages.*,users.login,users.avatar FROM  messages , users WHERE users.id = messages.id_author ORDER BY date DESC");
 while ($message = mysqli_fetch_assoc($res))
 {
+	if ($count % 2 == 0)
+		$pos = "left";
+	else
+		$pos = "right";
 	require('views/messages.phtml');
+	$count++;
 }
 // if (isset($_GET['id']))
 // {
