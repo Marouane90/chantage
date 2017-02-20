b97ddda51abcb8987de520b8be80715e4b402177
Marouane90 <marouane.ammar.90@gmail.com>
2017-02-20 10:42:25 +0100

    mise à jour

--- apps/create_message.php
+++ apps/create_message.php
@@ -1,8 +1,3 @@
 <?php
-$res = mysqli_query($db, "SELECT messages.*,users.login,users.avatar FROM  messages , users WHERE users.id = messages.id_author");
-while ($message = mysqli_fetch_assoc($res))
-{
-	require('views/messages.phtml');
-}
 require('views/create_message.phtml');
 ?>
--- apps/messages.php
+++ apps/messages.php
@@ -1,4 +1,9 @@
-// <?php
+<?php
+$res = mysqli_query($db, "SELECT messages.*,users.login,users.avatar FROM  messages , users WHERE users.id = messages.id_author");
+while ($message = mysqli_fetch_assoc($res))
+{
+	require('views/messages.phtml');
+}
 // if (isset($_GET['id']))
 // {
 // 	$id = intval($_GET['id']);
